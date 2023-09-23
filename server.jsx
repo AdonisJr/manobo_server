1fe841b2fa69863201ce47f6d35c1a6d859bd16d
unknown <adonisjr.suico@gmail.com>
2023-09-23 10:20:47 +0800

    First commit setting up express server

---
+++ server.jsx
@@ -0,0 +1,13 @@
+require('dotenv').config();
+const express = require('express');
+const app = express();
+const cors = require('cors');
+
+app.use(cors);
+app.use(express.json());
+app.use(express.urlencoded({extended: true}))
+
+
+app.listen(3001, () =>{
+    console.log(`Server is running at https://localhost:3001`);
+})
